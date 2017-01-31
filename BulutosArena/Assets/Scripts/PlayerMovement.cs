e74416c7fca6aa181aa3508d46c812e7e8c922f3
Veltouille <julienstatho@orange.fr>
2017-01-31 14:50:21 +0100

    Commit rotate Camera
    Modif code personnage pour manipuler la camera
    Reste a fixer le max rotateX a +90 et -90...

--- BulutosArena/Assets/Scripts/PlayerMovement.cs
+++ BulutosArena/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float moveSpeed;
+    public float sensitive = 2f;
+    public Camera eyes;
 
-	void Start () {
+    float rotateX;
+    float rotateY;
+
+    void Start () {
 		
 	}
 	
 	void Update () {
         MovePlayer();
+        RotateCamera();
+        
 	}
 
     private void MovePlayer()
     {
         transform.position += transform.forward * Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
         transform.position += transform.right * Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+
         //transform.position += transform.up * Input.GetAxisRaw("") * moveSpeed * Time.deltaTime;
+    }
+
+    private void RotateCamera()
+    {
+
+
+        rotateX = Input.GetAxis("Mouse X") * sensitive;
+        rotateY = Input.GetAxis("Mouse Y") * sensitive;
+
+        transform.Rotate(0, rotateX, 0);
+        eyes.transform.Rotate(-rotateY, 0, 0);
     }
 }
