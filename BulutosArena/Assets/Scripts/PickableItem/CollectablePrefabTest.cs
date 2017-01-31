e4d03877588a880b42d0f1b635f53ddf98739729
BuildTools <jonathan.navarro@etu.univ-amu.fr>
2017-01-31 17:17:02 +0100

    Commit de test pour branches

---
+++ BulutosArena/Assets/Scripts/PickableItem/CollectablePrefabTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablePrefabTest : CollectableBaseItem
+{
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Interracting with player <3");
+            base.OnTriggerEnter(other);
+        }
+    }
+}
