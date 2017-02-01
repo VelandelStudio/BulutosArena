ae75eb6ae496b601872c5e89a7810a81906e2d70
BuildTools <jonathan.navarro@etu.univ-amu.fr>
2017-02-01 14:48:51 +0100

    Ajout Network Manager

---
+++ BulutosArena/Assets/Scripts/Player/PlayerBaseScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool> {}
+
+public class PlayerBaseScript : NetworkBehaviour {
+
+    [SerializeField] ToggleEvent onToggleShared;
+    [SerializeField] ToggleEvent onToggleLocal;
+    [SerializeField] ToggleEvent onToggleRemote;
+
+    private GameObject mainCamera;
+
+    private void Start()
+    {
+        if (Camera.main != null)
+            mainCamera = Camera.main.gameObject;
+
+        EnablePlayer();
+    }
+
+    private void DisablePlayer()
+    {
+        if (mainCamera != null && isLocalPlayer)
+            mainCamera.SetActive(true);
+
+        onToggleShared.Invoke(false);
+
+        if (isLocalPlayer)
+            onToggleLocal.Invoke(false);
+        else
+            onToggleRemote.Invoke(false);
+    }
+
+    private void EnablePlayer()
+    {
+        if (mainCamera != null && isLocalPlayer)
+            mainCamera.SetActive(false);
+
+        onToggleShared.Invoke(true);
+
+        if(isLocalPlayer)
+            onToggleLocal.Invoke(true);
+        else
+            onToggleRemote.Invoke(true);
+    }
+}
